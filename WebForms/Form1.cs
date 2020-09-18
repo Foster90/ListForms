f6e7900417333ed6d5c6b00904724d26b9796eae
Matthew Foster <Matthew.Foster@Civica.co.uk>
2020-09-18 12:58:12 +0100

    List now moving back and forth

--- WebForms/Form1.cs
+++ WebForms/Form1.cs
@@ -29,31 +29,60 @@ namespace WebForms
 
         void MoveAll_OnClick1(object sender, EventArgs e)
         {
-            
+
             foreach (var item in listBox2.Items)
             {
-
-                listBox1.Items.Add(item);
+               listBox1.Items.Add(item);
             }
+
+            listBox2.Items.Clear();
 
         }
 
 
         void MoveAll2_OnClick1(object sender, EventArgs e)
         {
-            MessageBox.Show("Test");
+            foreach (var item in listBox1.Items)
+            {
+                listBox2.Items.Add(item);
+            }
+
+            listBox1.Items.Clear();
         }
 
 
         void Moveitem_OnClick1(object sender, EventArgs e)
         {
-            MessageBox.Show("Test");
+
+            ListBox.SelectedObjectCollection stem = listBox2.SelectedItems;
+            foreach (var item in stem)
+
+            {
+                listBox1.Items.Add(item);
+                
+            }
+            while (listBox2.SelectedItems.Count > 0)
+            {
+                listBox2.Items.Remove(listBox2.SelectedItems[0]);
+            }
+
+
         }
 
 
         void Moveitem2_OnClick1(object sender, EventArgs e)
         {
-            MessageBox.Show("Test");
+            ListBox.SelectedObjectCollection stem = listBox1.SelectedItems;
+            foreach (var item in stem)
+
+            {
+                listBox2.Items.Add(item);
+
+            }
+            while (listBox1.SelectedItems.Count > 0)
+            {
+                listBox1.Items.Remove(listBox1.SelectedItems[0]);
+            }
         }
 
 
