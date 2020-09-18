09c6cf4572c68eb73b13f8b534f588321cb21dec
Matthew Foster <Matthew.Foster@Civica.co.uk>
2020-09-18 10:32:39 +0100

    Update to list and buttons

--- WebForms/Form1.Designer.cs
+++ WebForms/Form1.Designer.cs
@@ -32,9 +32,9 @@
                   
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.MoveItem = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.MoveItem2 = new System.Windows.Forms.Button();
+            this.MoveAll = new System.Windows.Forms.Button();
+            this.MoveAll2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -58,6 +58,10 @@
             this.listBox2.Size = new System.Drawing.Size(195, 238);
             this.listBox2.TabIndex = 1;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            listBox2.Items.Add("Geeks");
+            listBox2.Items.Add("GFG");
+            listBox2.Items.Add("GeeksForGeeks");
+            listBox2.Items.Add("gfg");
             // 
             // button1
             // 
@@ -68,41 +72,41 @@
             this.MoveItem.Text = "Move Item";
             this.MoveItem.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // MoveItem2
             // 
-            this.button2.Location = new System.Drawing.Point(366, 361);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(197, 34);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.MoveItem2.Location = new System.Drawing.Point(366, 361);
+            this.MoveItem2.Name = "MoveItem2";
+            this.MoveItem2.Size = new System.Drawing.Size(197, 34);
+            this.MoveItem2.TabIndex = 3;
+            this.MoveItem2.Text = "MoveItem2";
+            this.MoveItem2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // MoveAll
             // 
-            this.button3.Location = new System.Drawing.Point(60, 401);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(197, 34);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.MoveAll.Location = new System.Drawing.Point(60, 401);
+            this.MoveAll.Name = "MoveAll";
+            this.MoveAll.Size = new System.Drawing.Size(197, 34);
+            this.MoveAll.TabIndex = 4;
+            this.MoveAll.Text = "MoveAll";
+            this.MoveAll.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // MoveAll2
             // 
-            this.button4.Location = new System.Drawing.Point(364, 401);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(197, 34);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.MoveAll2.Location = new System.Drawing.Point(364, 401);
+            this.MoveAll2.Name = "MoveAll2";
+            this.MoveAll2.Size = new System.Drawing.Size(197, 34);
+            this.MoveAll2.TabIndex = 5;
+            this.MoveAll2.Text = "MoveAll2";
+            this.MoveAll2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(668, 549);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.MoveAll2);
+            this.Controls.Add(this.MoveAll);
+            this.Controls.Add(this.MoveItem2);
             this.Controls.Add(this.MoveItem);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
@@ -123,13 +127,13 @@
         private System.Windows.Forms.Button MoveItem;
 
 
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button MoveItem2;
 
 
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button MoveAll;
 
 
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button MoveAll2;
     }
 }
 
